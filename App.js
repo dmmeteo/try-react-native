e3ebefb754abb29c3e332d0eefeab6a683945e3b
Dmitry Sichkar <dmmeteo@gmail.com>
2018-08-13 23:36:20 +0300

    state

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Robot from './components/Robot';
-import MultipleGreetings from './components/MultipleGreetings';
+import TextBlink from './components/TextBlink';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <MultipleGreetings/>
         <Robot/>
-        <Text>Open up App.js to start working on your app!</Text>
+        <TextBlink/>
       </View>
     );
   }
