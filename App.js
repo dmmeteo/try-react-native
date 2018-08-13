ea0603cffafc7c0400521bd63c7552696188c144
Dmitry Sichkar <dmmeteo@gmail.com>
2018-08-14 02:40:47 +0300

    TextInputs

--- App.js
+++ App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import FlexExample from './components/FlexExample';
+import { StyleSheet, TextInput, Text, View } from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={{flex: 1, flexDirection: 'column'}}>
-        <FlexExample/>
-      </View>
-    );
-  }
+    constructor(props){
+        super(props);
+        this.state = {
+            typedText: 'please type your text'
+        };
+    }
+
+    render() {
+        return (
+            <View>
+                <TextInput 
+                    style={styles.firstInput}
+                    keyboardType='email-address'
+                    placeholder='Enter your email'
+                    placeholderTextColor='red'
+                    underlineColorAndroid='transparent'
+                    onChangeText={
+                        (text) => {
+                            this.setState(
+                                (previouseState) => {
+                                    return {
+                                        typedText: text
+                                    }
+                                }
+                            )
+                        }
+                    }
+                />
+                <Text style={styles.firstText}>{this.state.typedText}</Text>
+                <TextInput
+                    style={styles.firstInput}
+                    keyboardType='default'
+                    secureTextEntry={true}
+                    placeholder='Enter your password'
+                    underlineColorAndroid='transparent'
+                />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  firstText: {
-    margin: 5,
-    color: 'green'
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    firstInput: {
+        height: 40,
+        margin: 20,
+        padding: 10,
+        borderColor: 'gray',
+        borderWidth: 1,
+    },
+    firstText: {
+        marginLeft: 20
+    }
 });
