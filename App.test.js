3c7ef84b3c80051e60b440ac5d7c948b0b4e0b8e
Dmitry Sichkar <dmmeteo@gmail.com>
2018-09-04 19:34:03 +0300

    TODOs

--- App.test.js
+++ App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import App from './App';
 
 import renderer from 'react-test-renderer';
-
+// TODO try react-native tests
 it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
